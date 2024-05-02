7c9067e6c3d9f58f4eb4d9d4bf1f800d02c4e977
RaulAstudillo06 <astudillo0606@gmail.com>
2024-05-01 19:53:40 -0700

    Update Rastrigin runner

--- experiments/rastrigin_runner.py
+++ experiments/rastrigin_runner.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from typing import Callable
 
 import os
 import numpy as np
@@ -7,7 +6,6 @@ import sys
 import torch
 import argparse
 import json
-from botorch.acquisition.analytic import PosteriorMean
 from botorch.settings import debug
 from botorch.test_functions.synthetic import Rastrigin
 from torch import Tensor
@@ -21,9 +19,10 @@ print(script_dir[:-12])
 sys.path.append(script_dir[:-12])
 
 from src.bax.alg.evolution_strategies import EvolutionStrategies
+from src.bax.alg.lbfgsb import LBFGSB
 from src.bax.util.domain_util import unif_random_sample_domain
 from src.experiment_manager import experiment_manager
-from src.performance_metrics import ObjValAtMaxPostMean, BestValue
+from src.performance_metrics import BestValue
 from src.utils import compute_noise_std
 
 
@@ -31,61 +30,64 @@ from src.utils import compute_noise_std
 # use argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--policy', type=str, default='ps')
-parser.add_argument('--trials', type=int, default=5)
-parser.add_argument('--dim', type=int, default=5)
+parser.add_argument('--dim', type=int, default=10)
 parser.add_argument('--trials', type=int, default=5)
 parser.add_argument('--first_trial', type=int, default=1)
 parser.add_argument('--noise', type=float, default=0.0)
-parser.add_argument('--batch_size', type=int, default=3)
-parser.add_argument('--max_iter', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--max_iter', type=int, default=200)
 parser.add_argument('--samp_str', type=str, default='mut')
 parser.add_argument('--model_type', type=str, default='gp')
 parser.add_argument('--save', '-s', action='store_true', default=False)
 parser.add_argument('--restart', '-r', action='store_true', default=False)
 args = parser.parse_args()
 
+# Objective function
+n_dim = args.dim
 
-# === To RUN === # 
-# python rastrigin_runner.py -s --dim 10 --trials 10 --policy bax
-bounds = [-5.12, 5.12]
 def obj_func(X: Tensor) -> Tensor:
     if isinstance(X, np.ndarray):
         X = torch.tensor(X)
-    f = Rastrigin(negate=True, dim=args.dim) # negate=True is for maximization
-    X_rescaled = (bounds[1] - bounds[0]) * X + bounds[0]
-    objective_X = f(X_rescaled)
+    rastrigin = Rastrigin(negate=True, dim=n_dim) # negate=True is for maximization
+    objective_X = rastrigin(10.24 * X - 5.12)
     return objective_X
 
 
-# Set algorithm details
-n_dim = args.dim
-domain = [[0, 1]] * n_dim
-init_x = unif_random_sample_domain(domain, n=1)
+# Algorithm
+algo_id = "lbfgsb"
+
+if algo_id == "cma":
+    domain = [[0, 1]] * n_dim
+    init_x = [[0.0] * n_dim]
+
+    algo_params = {
+        "n_generation": 100 * n_dim,
+        "n_population": 10,
+        "samp_str": args.samp_str,
+        "init_x": init_x[0],
+        "domain": domain,
+        "crop": True,
+        "opt_mode": "max",
+    }
+    algo = EvolutionStrategies(algo_params)
+elif algo_id == "lbfgsb":
+    num_restarts = 5 * n_dim
+    raw_samples = 100 * n_dim
+
+    algo_params = {
+        "name" : "LBFGSB",
+        "opt_mode" : "max",
+        "n_dim" : n_dim,
+        "num_restarts" : num_restarts,
+        "raw_samples" : raw_samples,
+    }
+    algo = LBFGSB(algo_params)
 
 
-algo_params = {
-    "n_generation": 50,
-    "n_population": 10,
-    "samp_str": args.samp_str,
-    "init_x": init_x[0],
-    "domain": domain,
-    "normal_scale": 0.05,
-    "keep_frac": 0.3,
-    "crop": False,
-    "opt_mode": "max",
-    'crop': True,
-}
-algo = EvolutionStrategies(algo_params)
-
+# Performance metric
 algo_metric = algo.get_copy()
 performance_metrics = [
-    # ObjValAtMaxPostMean(obj_func, input_dim),
-    BestValue(
-        algo_metric, 
-        obj_func,
-        eval_mode="best_value",
-        num_runs=5,
-    ),
+    BestValue(algo_metric, obj_func),
 ]
 
 problem = "rastrigin" + f"_{n_dim}d"
@@ -121,7 +123,7 @@ experiment_manager(
     input_dim=n_dim,
     noise_type=noise_type,
     noise_level=noise_levels,
-    policy=args.policy + f"_model{args.model_type}" + f"_{args.samp_str}",
+    policy=args.policy + f"_{args.model_type}" + f"_{algo_id}",
     batch_size=args.batch_size,
     num_init_points=2 * (n_dim + 1),
     num_iter=args.max_iter,
@@ -129,6 +131,7 @@ experiment_manager(
     last_trial=last_trial,
     restart=False,
     model_type=args.model_type,
-    save_data=args.save,
-    bax_num_cand=5000,
+    save_data=True,
+    bax_num_cand=1000 * n_dim,
+    exe_paths=30,
 )
