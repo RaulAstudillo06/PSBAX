7e686df4887237a02e9d451a05f6636d64a8e6aa
ClorisCheng <cloris_cheng@outlook.com>
2024-05-03 02:03:45 +0000

    fix shell runner files for discobax

--- experiments/run_discobax_ps.sh
+++ experiments/run_discobax_ps.sh
@@ -4,7 +4,7 @@ pidfile="discobax_pids_ps.txt"  # File where PIDs will be stored
 # Clean the file at the start of the script
 > "$pidfile"
 
-for acq_func in "bax"
+for acq_func in "OPT" "ps"
 do
     for first_trial in 1 3 5 7 9
     do
