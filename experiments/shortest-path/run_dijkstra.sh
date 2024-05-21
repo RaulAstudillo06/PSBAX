222b838c5abb4c2e45e0de1d6a23fc83886e7e80
ClorisCheng <cloris_cheng@outlook.com>
2024-05-21 04:30:32 +0000

    new california shell file

--- experiments/shortest-path/run_dijkstra.sh
+++ experiments/shortest-path/run_dijkstra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for acq_func in "bax" "ps" "random"
+for acq_func in "ps" "random" "bax"
 do
-    python new_california_runner.py -s --trials 10 --policy $acq_func &
+    python new_california_runner.py -s --trials 30 --policy $acq_func &
 done
 wait
