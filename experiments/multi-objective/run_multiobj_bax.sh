170ac06d07edb29170009e8628e461db1f6f474e
ClorisCheng <cloris_cheng@outlook.com>
2024-11-03 18:04:50 +0000

    remove classes not used

--- experiments/multi-objective/run_multiobj_bax.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for first_trial in 1 6
-do
-    # Run the command in the background
-    python multiobjective_runner.py -s --policy bax --problem zdt2 --noise 0.1 --n_dim 6 --n_obj 2 --n_gen 500 --pop_size 100 --max_iter 50 --batch_size 5 --first_trial $first_trial & 
-    # python multiobjective_runner.py -s --policy bax --problem dtlz2 --noise 0 --n_dim 6 --n_obj 2 --n_gen 500 --pop_size 100 --max_iter 50 --batch_size 5 --first_trial $first_trial &
-done
-# Wait for all background jobs to finish
-wait
--- experiments/shortest-path/run_dijkstra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for acq_func in "ps" "random" "bax"
-do
-    python new_california_runner.py -s --trials 30 --policy $acq_func &
-done
-wait
